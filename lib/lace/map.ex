7146fa22918196c6ee4ea49d57a145cc4730b888
Leonardo Donelli <leo@wideknot.me>
2024-08-06 00:57:35 +0100

    Add Lace.Map.put_if/3,4

--- lib/lace/map.ex
+++ lib/lace/map.ex
@@ -50,4 +50,58 @@ defmodule Lace.Map do
       false -> move(map, old_key, new_key)
     end
   end
+
+  @doc """
+  Puts the given `value` in `map` under `key` if `value` is non-`nil`.
+
+  Useful to conditionally add non-empty fields to maps,
+  in a pipeline-friendly way and without having to do a second pass
+  to reject fields with empty values.
+
+  If you need custom logic to decide whether to add the value,
+  use `put_if/4`, which takes a predicate function.
+
+  ## Example
+
+      iex> fetch_user = fn _user_id -> nil end
+      ...> fetch_project = fn project_id -> "project-" <> to_string(project_id) end
+      ...>
+      ...> %{}
+      ...> |> Lace.Map.put_if(:user, fetch_user.(23))
+      ...> |> Lace.Map.put_if(:project, fetch_project.(13))
+      %{project: "project-13"}
+  """
+  def put_if(map, key, value) do
+    case value do
+      nil -> map
+      _ -> Map.put(map, key, value)
+    end
+  end
+
+  @doc """
+  Conditionally adds a value to the map based on a predicate function.
+
+  This function is similar to `put_if`, but allows for more complex
+  logic by using a predicate function that takes two parameters: the
+  current map and the value to be added. The predicate should return
+  a boolean indicating whether the value should be inserted into the map.
+
+  ## Examples
+
+      iex> Lace.Map.put_if(%{a: 1}, :b, 2 + 3, fn _map, n -> n > 4 end)
+      %{a: 1, b: 5}
+
+      iex> Lace.Map.put_if(%{a: 1}, :b, 2, fn _, _ -> :rand.uniform(3) < 5 end)
+      %{a: 1, b: 2}
+
+      iex> Lace.Map.put_if(%{a: 1}, :b, 2, fn _, _ -> :rand.uniform(3) > 5 end)
+      %{a: 1}
+  """
+  @spec put_if(map(), Map.key(), Map.value(), (map(), Map.value() -> boolean())) :: map()
+  def put_if(map, key, value, predicate_fn) do
+    case predicate_fn.(map, value) do
+      false -> map
+      true -> Map.put(map, key, value)
+    end
+  end
 end
